3cc1886301b8f98a4f374d534853f074a47f0e54
Narutep Wongsaita <natep.boat000@gmail.com>
2022-08-27 11:46:06 +0700

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld{
-public static void main(String[] arg0){
-System.out.println("=========================================\n");
-System.out.println("I'm very glad to say Hello to the World.");
-System.out.println("\n=========================================");
-}
-}
